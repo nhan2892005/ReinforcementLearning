270b7c67e03d535cc9d3c02030ad9f7e63947188
nhan2892005 <143471539+nhan2892005@users.noreply.github.com>
2024-11-05 22:55:34 +0700

    Update: Greedy Sorted

--- CSP/student_submissions/s2210xxx/policy2210xxx.py
+++ CSP/student_submissions/s2210xxx/policy2210xxx.py
@@ -3,5 +3,45 @@ import numpy as np
 import random
 
 class Policy2210xxx(Policy):
+    def __init__(self):
+        pass
 
+    def get_action(self, observation, info):
+        list_prods = observation["products"]
+        list_prods = sorted(list_prods, key=lambda x: x["size"][0] * x["size"][1], reverse=True)
+
+        prod_size = [0, 0]
+        stock_idx = -1
+        pos_x, pos_y = 0, 0
+
+        # Pick a product that has quality > 0
+        for prod in list_prods:
+            if prod["quantity"] > 0:
+                prod_size = prod["size"]
+
+                # Loop through all stocks
+                for i, stock in enumerate(observation["stocks"]):
+                    stock_w, stock_h = self._get_stock_size_(stock)
+                    prod_w, prod_h = prod_size
+
+                    if stock_w < prod_w or stock_h < prod_h:
+                        continue
+
+                    pos_x, pos_y = None, None
+                    for x in range(stock_w - prod_w + 1):
+                        for y in range(stock_h - prod_h + 1):
+                            if self._can_place_(stock, (x, y), prod_size):
+                                pos_x, pos_y = x, y
+                                break
+                        if pos_x is not None and pos_y is not None:
+                            break
+
+                    if pos_x is not None and pos_y is not None:
+                        stock_idx = i
+                        break
+
+                if pos_x is not None and pos_y is not None:
+                    break
+
+        return {"stock_idx": stock_idx, "size": prod_size, "position": (pos_x, pos_y)}
         
